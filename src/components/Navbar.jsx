cbdad71be86e5eaaa1abbf5c4a3c0eac201f53d4
AgiestaNazwa <150083519+AgiestaNazwa@users.noreply.github.com>
2023-11-08 14:45:26 +0700

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar(){
+    return (
+        <div className="navbar">
+            <h1>My Web</h1>
+            <div className="items">
+                <p>Home</p>
+                <p>Gallery</p>
+                <p>About Me</p>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
